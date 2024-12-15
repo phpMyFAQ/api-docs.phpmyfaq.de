e66cb37ccceff18aff7f76eb9566265144783e81
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-12-15 11:07:44 +0100

    chore: updated depedencies, use 4.0 branch for OpenAPI docs

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import 'swagger-ui-react/swagger-ui.css';
 
 export default function Home() {
   return (
-    <SwaggerUI url="https://raw.githubusercontent.com/thorsten/phpMyFAQ/main/docs/openapi.json" />
+    <SwaggerUI url="https://raw.githubusercontent.com/thorsten/phpMyFAQ/4.0/docs/openapi.json" />
   );
 }
